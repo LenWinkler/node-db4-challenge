d1af4303a38b634312faeb26a6f047eb7cdf5975
LenWinkler <lenwinkler86@gmail.com>
2019-11-08 00:12:44 -0500

    wrote data access functions

---
+++ recipes/recipes-model.js
@@ -0,0 +1,23 @@
+const db = require('../data/db-config');
+
+module.exports = {
+    getRecipes,
+    getShoppingList,
+    getInstructions
+}
+
+function getRecipes() {
+    return db('recipes')
+};
+
+function getShoppingList(recipe_id) {
+    return db('ingredients')
+    .where('recipe_id', recipe_id)
+};
+
+function getInstructions(recipe_id) {
+    return db.select('step_number', 'instructions')
+            .from('steps')
+            .where('recipe_id', recipe_id)
+            .orderBy('step_number')
+}
